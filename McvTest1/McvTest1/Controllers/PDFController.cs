128e39ed257d4584a241ae2c00adad61005bef08
Andong.Li <43534545@qq.com>
2012-08-03 17:47:04 -0400

    First build

---
+++ McvTest1/McvTest1/Controllers/PDFController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using McvTest1.Models;
+
+namespace McvTest1.Controllers
+{
+    public class PDFController : Controller
+    {
+        //
+        // GET: /PDF/
+
+        PDFDbContext db = new PDFDbContext();
+
+        public ActionResult Index()
+        {
+            var pdfs = from p in db.PDFs
+            //var pdfs = from p in db.dbe8a4fca9b69347d69e47a09701772bdd
+                       where p.uploadTime > new DateTime(2007, 08, 30)
+                       select p;
+            return View(pdfs.ToList());
+        }
+
+    }
+}
---
+++ McvTest1/McvTest1/Models/PDF.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+
+namespace McvTest1.Models
+{
+    public class PDF
+    {
+        public int ID { get; set; }
+        public String Title{ get; set; }
+        public String Author { get; set; }
+        public DateTime uploadTime { get; set; }
+    }
+
+    public class PDFDbContext : DbContext
+    {
+        public DbSet<PDF> PDFs { get; set; }
+        //public DbSet<PDF> dbe8a4fca9b69347d69e47a09701772bdd { get; set; }
+    }
+}
